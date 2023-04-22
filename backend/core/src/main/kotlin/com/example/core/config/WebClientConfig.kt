ecc14c9c9159b8485ac0bc36002c2bb4f6e560ce
bekurin <bekurin17@gmail.com>
2023-04-22 17:56:57 +0900

    feat: apply exception handler to http interface

--- backend/core/src/main/kotlin/com/example/core/config/WebClientConfig.kt
+++ backend/core/src/main/kotlin/com/example/core/config/WebClientConfig.kt
@@ -1,8 +1,10 @@
 package com.example.core.config
 
+import com.example.core.exception.InternalServerException
 import com.example.core.service.HelloService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatusCode
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.support.WebClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
@@ -12,7 +14,12 @@ class WebClientConfig {
 
     @Bean
     fun helloService(): HelloService {
-        val client = WebClient.create("<domain>")
+        val client = WebClient.builder()
+            .defaultStatusHandler(HttpStatusCode::isError) { response ->
+                response.bodyToMono(InternalServerException::class.java)
+            }
+            .baseUrl("<baseUrl>")
+            .build()
         val serviceProxyFactory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build()
         return serviceProxyFactory.createClient(HelloService::class.java)
     }
